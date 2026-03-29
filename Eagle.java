aec0ecb8f24c612db3b392dc82204b2fa3180c03
BurakHINGE <burakmentese16@gmail.com>
2026-03-29 19:31:16 +0300

    Create Eagle.java
    Eagle has been created.

---
+++ Eagle.java
@@ -0,0 +1,16 @@
+class Eagle extends Animals implements Flyable{
+    
+    public Eagle (String name, int age, double weight) {
+        super(name, age, weight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Gaaaakkk");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Kartal uçuyor.");
+    }
+}
