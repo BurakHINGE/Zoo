7655635d2080e68c2674f84e8b8b2c61670322b1
BurakHINGE <burakmentese16@gmail.com>
2026-03-29 19:31:08 +0300

    Create Dolphin.java
    Dolphin has been created.

---
+++ Dolphin.java
@@ -0,0 +1,16 @@
+class Dolphin extends Animals implements Swimmable{
+    
+    public Dolphin (String name, int age, double weight) {
+        super(name, age, weight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Ee-eee-eee");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Yunus yüzüyor.");
+    }
+}
