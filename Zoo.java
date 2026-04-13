fe2d2a50ba2366ad774f197b3e018ec99b2b393c
BurakHINGE <burakmentese16@gmail.com>
2026-04-13 22:53:47 +0300

    Update Zoo.java
    Scanner has been implemented.

--- Zoo.java
+++ Zoo.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class Zoo {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
         
         Animals[] zoo = new Animals[5];
         zoo[0] = new Lion("Simba", 5, 190.5);
@@ -8,12 +11,25 @@ public class Zoo {
         zoo[3] = new Lion("Nala", 4, 180.0);
         zoo[4] = new Eagle("Sky", 2, 4.8);
 
+        System.out.println("===== Hayvanat Bahçesine Hoş Geldin =====");
+        System.out.println("Hangi hayvanlara bakmak istersin?\n1 - Aslanlar\n2 - Kartallar\n3 - Yunuslar");
+        int choice = input.nextInt();
+
+        if (choice == 1) {
+            for (Animals a : zoo) {
+                if (a instanceof Lion) {
+                    ((Runnable) a).run();
+                    a.makeSound();
+                }
+            }
+        }
+
         System.out.println("=== Hayvan Sesleri ===");
         for (Animals a : zoo) {
             a.makeSound();
         }
 
-        System.out.println("=== Hayvanlar Ne Yapıyor");
+        System.out.println("=== Hayvanlar Ne Yapıyor ===");
         for (Animals a : zoo) {
             if (a instanceof Runnable) {
                 ((Runnable) a).run();
