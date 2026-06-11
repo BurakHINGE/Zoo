94f4dfa834347886c48c8d5e7270d75f5aac7782
BurakHINGE <burakmentese16@gmail.com>
2026-06-11 23:13:10 +0300

    Create Shark.java
    Shark has been created.

---
+++ src/Shark.java
@@ -0,0 +1,16 @@
+class Shark extends Animals implements Swimmable{
+    
+    public Shark (String name, int age, double weight) {
+        super(name, age, weight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Ee-eee-eee");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(getName() + " yüzüyor.");
+    }
+}
